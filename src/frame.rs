111d491822db282d432f607d909e660e8bb4f5dc
猫猫 <overflowcat@gmail.com>
2023-06-18 06:49:19 +0800

    refactor: use Arc<[T]> instead of Vec<T>

--- src/frame.rs
+++ src/frame.rs
@@ -3,6 +3,7 @@ use paddleocr::ContentData;
 use std::fmt::{Display, Result as FmtResult};
 use std::fs::canonicalize;
 use std::path::PathBuf;
+use std::sync::Arc;
 use std::time::Duration;
 
 use crate::ocr::ocr::ocr_and_parse;
@@ -42,26 +43,17 @@ impl ShuukanFrame {
         Ok(ShuukanFrame { path, time })
     }
 
-    pub fn ocr(&self) -> Result<Vec<ContentData>, String> {
+    pub fn ocr(&self) -> Result<Arc<[ContentData]>, String> {
         ocr_and_parse(
             canonicalize(self.path.clone())
                 .or_else(|e| Err(e.to_string()))?
                 .to_str()
                 .unwrap(),
-        )
+        ).and_then(|x| Ok(x.into()))
     }
 
-    pub fn get_info(&self) -> Result<ShuukanVideoInfo, String> {
-        let ocr_result = self.ocr()?;
-        let is_single_video = ocr_result
-            .iter()
-            .filter(|x| x.text.contains("投稿"))
-            .next()
-            .is_none();
-        if is_single_video {
-            print!("<不是单个视频>")
-        }
-        let vid = ocr_result
+    fn find_vid(ocr_result: &[ContentData]) -> Result<Vid, String> {
+        ocr_result
             .iter()
             .filter_map(|x| {
                 // lower x starts with av or bv
@@ -80,8 +72,22 @@ impl ShuukanFrame {
                 }
             })
             .next()
-            .ok_or("No vid found")?;
-        let title = ocr_result
+            .ok_or("No vid found".into())
+    }
+
+    fn find_time(ocr_result: &[ContentData]) -> Result<NaiveDate, String> {
+        ocr_result
+            .iter()
+            .filter_map(|x| {
+                // yyyy-mm-dd hh:mm
+                NaiveDate::parse_from_str(&x.text, "%Y-%m-%d %H:%M").ok()
+            })
+            .next()
+            .ok_or("No time found".into())
+    }
+
+    fn find_title(ocr_result: &[ContentData]) -> Result<String, String> {
+        Ok(ocr_result
             .iter()
             .filter(|x| {
                 let left = x.rect[0][0];
@@ -90,17 +96,23 @@ impl ShuukanFrame {
                 left < 150 && top < 1001 && bottom > 1001
             })
             .next()
-            .ok_or("No title found")?
-            .text
-            .to_string();
-        let time = ocr_result
+            .ok_or("No title found, maybe bangumi")?
+            .text.clone())
+    }
+
+    pub fn get_info(&self) -> Result<ShuukanVideoInfo, String> {
+        let ocr_result = self.ocr()?;
+        let is_single_video = ocr_result
             .iter()
-            .filter_map(|x| {
-                // yyyy-mm-dd hh:mm
-                NaiveDate::parse_from_str(&x.text, "%Y-%m-%d %H:%M").ok()
-            })
+            .filter(|x| x.text.contains("投稿"))
             .next()
-            .ok_or("No time found")?;
+            .is_none();
+        if is_single_video {
+            print!("<不是单个视频> ")
+        }
+        let vid = Self::find_vid(&ocr_result)?;
+        let title = Self::find_title(&ocr_result)?;
+        let time = Self::find_time(&ocr_result)?;
 
         Ok(ShuukanVideoInfo {
             vid,
